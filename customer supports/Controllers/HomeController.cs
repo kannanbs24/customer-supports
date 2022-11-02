e72f1b95bb35113b0cdb81734b808b470f70a00e
kannan <kannanbsk1609080@gmail.com>
2022-11-02 14:00:08 +0530

    Add project files.

---
+++ customer supports/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using customer_supports.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace customer_supports.Controllers
+{
+    public class HomeController : Controller
+    {
+        phase4Entities db = new phase4Entities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Index(userinfo log)
+        {
+            var user = db.userinfoes.Where(x => x.userid == log.userid && x.passwords == log.passwords).Count();
+            if (user > 0)
+            {
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult Dashboard()
+        {
+            custinfo c=new custinfo();
+            return View(c);
+        }
+        [HttpPost]
+        public ActionResult Dashboard(custinfo cust)
+        {
+            custinfo cs = new custinfo();
+            cs.logid = cust.logid;
+            cs.customer_email = cust.customer_email;    
+            cs.customer_name = cust.customer_name;
+            cs.log_status=cust.log_status;
+            cs.descriptions = cust.descriptions;
+            db.custinfoes.Add(cs);
+            db.SaveChanges();
+            Response.Write("<script> alert('complaint registered successfully')</script>");
+            return View();
+
+        }
+    }
+}
---
+++ customer supports/customer support test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+using System.Web.Mvc;
+using customer_supports.Controllers;
+using customer_supports.Models;
+
+namespace customer_supports
+{
+    [TestFixture]
+    public class customer_support_class
+    {
+        [TestCase]
+       public void userinfo_test()       
+        {
+           phase4Entities db=new phase4Entities();
+
+            var found = db.userinfoes.ToList();
+            
+            Assert.AreEqual(1,found[0].userid);
+            Assert.AreEqual("kannan@123", found[2].passwords);
+        }
+        
+    }
+}
